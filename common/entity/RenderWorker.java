a656c62c8a98882a0d84050e0375614f5204b851
dewfalse <dewfalse@gmail.com>
2012-12-01 14:47:57 +0900

    フォーマット

--- common/entity/RenderWorker.java
+++ common/entity/RenderWorker.java
@@ -39,55 +39,58 @@ public class RenderWorker extends RenderLiving {
     }
 
     protected void renderEquippedItems(EntityLiving entityliving, float f)
-    {
-        if (entityliving instanceof EntityWorker)
-        {
-        	EntityWorker ea = ((EntityWorker)entityliving);
-            int ii = ea.getInventoryType();
+ {
+		if (entityliving instanceof EntityWorker) {
+			EntityWorker ea = ((EntityWorker) entityliving);
+			int ii = ea.getInventoryType();
 
-            if (ii > 0)
-            {
-                ItemStack itemstack = new ItemStack(ii, 1, ea.getInventoryDamage());
-                //if(itemstack.itemID!=0){
-                GL11.glPushMatrix();
-                model.light.postRender(0.0625F);
-                GL11.glTranslatef(-0.0625F, 0.4375F, 0.0625F);
+			if (ii > 0) {
+				ItemStack itemstack = new ItemStack(ii, 1,
+						ea.getInventoryDamage());
+				// if(itemstack.itemID!=0){
+				GL11.glPushMatrix();
+				model.light.postRender(0.0625F);
+				GL11.glTranslatef(-0.0625F, 0.4375F, 0.0625F);
 
-                if (itemstack.itemID < 256 && RenderBlocks.renderItemIn3d(Block.blocksList[itemstack.itemID].getRenderType()))
-                {
-                    float f1 = 0.3F;
-                    GL11.glTranslatef(0.0875F, -0.9F, -0.1F);//GL11.glTranslatef(0.0F, -0.1875F, -0.3125F);
-                    //f1 *= 0.75F;
-                    //GL11.glRotatef(20F, 1.0F, 0.0F, 0.0F);
-                    GL11.glScalef(f1, -f1, f1);
-                }
-                else if (Item.itemsList[itemstack.itemID].isFull3D())
-                {
-                    float f2 = 0.3125F;
-                    GL11.glTranslatef(0.1F, -0.8F, 0.4F);
-                    GL11.glScalef(f2, -f2, f2);
-                    GL11.glRotatef(-80F, 1.0F, 0.0F, 0.0F);
-                    GL11.glRotatef(45F, 0.0F, 1.0F, 0.0F);
-                }
-                else
-                {
-                    /* float f3 = 0.375F;
-                     GL11.glTranslatef(0.25F, -0.1875F, -0.1875F);
-                     GL11.glScalef(f3, f3, f3);*/
-                    float f2 = 0.3125F;
-                    GL11.glTranslatef(0.1F, -0.8F, 0.4F);
-                    GL11.glScalef(f2, -f2, f2);
-                    GL11.glRotatef(-80F, 1.0F, 0.0F, 0.0F);
-                    GL11.glRotatef(45F, 0.0F, 1.0F, 0.0F);
-                    /*GL11.glRotatef(60F, 0.0F, 0.0F, 1.0F);
-                    GL11.glRotatef(-90F, 1.0F, 0.0F, 0.0F);
-                    GL11.glRotatef(20F, 0.0F, 0.0F, 1.0F);*/
-                }
+				if (itemstack.itemID < 256
+						&& RenderBlocks
+								.renderItemIn3d(Block.blocksList[itemstack.itemID]
+										.getRenderType())) {
+					float f1 = 0.3F;
+					GL11.glTranslatef(0.0875F, -0.9F, -0.1F);// GL11.glTranslatef(0.0F,
+																// -0.1875F,
+																// -0.3125F);
+					// f1 *= 0.75F;
+					// GL11.glRotatef(20F, 1.0F, 0.0F, 0.0F);
+					GL11.glScalef(f1, -f1, f1);
+				} else if (Item.itemsList[itemstack.itemID].isFull3D()) {
+					float f2 = 0.3125F;
+					GL11.glTranslatef(0.1F, -0.8F, 0.4F);
+					GL11.glScalef(f2, -f2, f2);
+					GL11.glRotatef(-80F, 1.0F, 0.0F, 0.0F);
+					GL11.glRotatef(45F, 0.0F, 1.0F, 0.0F);
+				} else {
+					/*
+					 * float f3 = 0.375F; GL11.glTranslatef(0.25F, -0.1875F,
+					 * -0.1875F); GL11.glScalef(f3, f3, f3);
+					 */
+					float f2 = 0.3125F;
+					GL11.glTranslatef(0.1F, -0.8F, 0.4F);
+					GL11.glScalef(f2, -f2, f2);
+					GL11.glRotatef(-80F, 1.0F, 0.0F, 0.0F);
+					GL11.glRotatef(45F, 0.0F, 1.0F, 0.0F);
+					/*
+					 * GL11.glRotatef(60F, 0.0F, 0.0F, 1.0F);
+					 * GL11.glRotatef(-90F, 1.0F, 0.0F, 0.0F);
+					 * GL11.glRotatef(20F, 0.0F, 0.0F, 1.0F);
+					 */
+				}
 
-                renderManager.itemRenderer.renderItem(entityliving, itemstack, 0);
-                GL11.glPopMatrix();
-            }
-        }
-    }
+				renderManager.itemRenderer.renderItem(entityliving, itemstack,
+						0);
+				GL11.glPopMatrix();
+			}
+		}
+	}
 
 }
