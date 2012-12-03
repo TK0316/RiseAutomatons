19bfebaa7e5571e3caa00fd6a4e31d2b005dfb42
dewfalse <dewfalse@gmail.com>
2012-12-03 21:31:33 +0900

    Workerブロッククラスを追加

---
+++ common/block/BlockWorker.java
@@ -0,0 +1,15 @@
+package riseautomatons.common.block;
+
+import net.minecraft.src.BlockContainer;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.World;
+
+public class BlockWorker extends BlockContainer {
+
+	@Override
+	public TileEntity createNewTileEntity(World var1) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
