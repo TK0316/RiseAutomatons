01676798e4070da086344ac116c90751fc0be9c7
dewfalse <dewfalse@gmail.com>
2012-12-09 08:51:49 +0900

    追加したブロックを登録

--- common/block/Blocks.java
+++ common/block/Blocks.java
@@ -4,7 +4,11 @@ import riseautomatons.common.Ids;
 import riseautomatons.common.OreGenerator;
 import riseautomatons.common.RiseAutomatons;
 import riseautomatons.common.entity.EntityWorker;
+import riseautomatons.common.item.ItemArch;
+import riseautomatons.common.item.ItemComplex;
+import riseautomatons.common.item.ItemLumo;
 import net.minecraft.src.Block;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.Material;
 import net.minecraft.src.ModLoader;
 import net.minecraftforge.client.MinecraftForgeClient;
@@ -17,6 +21,8 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 public class Blocks {
 	public static final String BLOCK_PNG = "/riseautomatons/block.png";
 	public static final String PATTERNS_PNG = "/riseautomatons/patterns.png";
+	public static int skyIconIndex = 13;
+	public static int walkIconIndex  = 14;
 
 	public static void init() {
 		GameRegistry.registerBlock(worker);
@@ -43,6 +49,29 @@ public class Blocks {
 		LanguageRegistry.addName(saltOre, "Salt Ore");
 		GameRegistry.registerBlock(sulfOre);
 		LanguageRegistry.addName(sulfOre, "Sulfide");
+		GameRegistry.registerBlock(tech, ItemComplex.class);
+		LanguageRegistry.addName(new ItemStack(tech, 1, 0), "Ancient Construct");
+		LanguageRegistry.addName(new ItemStack(tech, 1, 1), "Bionic Mass");
+		LanguageRegistry.addName(new ItemStack(tech, 1, 2), "Domestic Tile");
+		LanguageRegistry.addName(new ItemStack(tech, 1, 3), "Domestic Tile2");
+		GameRegistry.registerBlock(sky, ItemLumo.class);
+		LanguageRegistry.addName(new ItemStack(sky, 1, 0), "Atmospheric simulator");
+		LanguageRegistry.addName(new ItemStack(sky, 1, 1), "Walkway Tile");
+		GameRegistry.registerBlock(arch, ItemArch.class);
+		LanguageRegistry.addName(arch, "Architectural Shell");
+		GameRegistry.registerBlock(architect);
+		LanguageRegistry.addName(architect, "Architect");
+		ModLoader.registerTileEntity(TileEntityArchitect.class, "Architect",
+				new TileEntityArchitectRenderer());
+		GameRegistry.registerBlock(archBend);
+		LanguageRegistry.addName(archBend, "Architectural Connector");
+		GameRegistry.registerBlock(crink);
+		LanguageRegistry.addName(crink, "Foliage Array");
+		GameRegistry.registerBlock(frass);
+		LanguageRegistry.addName(frass, "Frass");
+
+		GameRegistry.registerBlock(teleporter);
+		LanguageRegistry.addName(teleporter, "Teleporter");
 
 		GameRegistry.registerWorldGenerator(new OreGenerator());
 		registerTextures();
@@ -89,4 +118,34 @@ public class Blocks {
 			.setHardness(3F).setResistance(5F)
 			.setStepSound(Block.soundStoneFootstep).setBlockName("SaltOre")
 			.setCreativeTab(RiseAutomatons.tabAutomatons);
+	public static Block tech = (new BlockComplex(Ids.blockTech))
+			.setHardness(1.0F).setResistance(5F)
+			.setStepSound(Block.soundStoneFootstep).setBlockName("tech")
+			.setCreativeTab(RiseAutomatons.tabAutomatons);
+	public static Block arch = (new BlockArch(Ids.blockArch))
+			.setHardness(0.25F).setStepSound(Block.soundGlassFootstep)
+			.setBlockName("arch").setCreativeTab(RiseAutomatons.tabAutomatons);
+	public static Block sky = (new BlockLumo(Ids.blockSky)).setHardness(1F)
+			.setResistance(5F).setLightValue(1F)
+			.setStepSound(Block.soundGlassFootstep).setBlockName("sky")
+			.setCreativeTab(RiseAutomatons.tabAutomatons);
+	public static Block archBend = (new BlockArchBend(Ids.blockArchBend))
+			.setHardness(0.15F).setStepSound(Block.soundGlassFootstep)
+			.setBlockName("archbend").setCreativeTab(RiseAutomatons.tabAutomatons);
+	public static Block architect = (new BlockArchitect(Ids.blockArchitect))
+			.setBlockUnbreakable().setStepSound(Block.soundStoneFootstep)
+			.setBlockName("coreMachine")
+			.setCreativeTab(RiseAutomatons.tabAutomatons);
+	public static Block crink = (new BlockCrink(Ids.blockCrink, 234))
+			.setHardness(0.1F).setLightOpacity(1).setLightValue(0.5F)
+			.setStepSound(Block.soundGlassFootstep).setBlockName("crink")
+			.setCreativeTab(RiseAutomatons.tabAutomatons);
+    public static Block frass = (new BlockFrass(Ids.blockFrass))
+			.setTextureFile(BLOCK_PNG)
+    		.setHardness(0.25F).setStepSound(Block.soundGlassFootstep).setBlockName("frass")
+    		.setCreativeTab(RiseAutomatons.tabAutomatons);
+
+	public static Block teleporter = (new BlockTeleporter())
+			.setCreativeTab(RiseAutomatons.tabAutomatons);
+
 }
