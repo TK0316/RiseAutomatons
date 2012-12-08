287af587c431495ad41e792a9d4cfc4e7a138f30
dewfalse <dewfalse@gmail.com>
2012-12-09 08:34:36 +0900

    Techバイオームの木を追加

---
+++ common/world/BiomeGenTech.java
@@ -0,0 +1,31 @@
+package riseautomatons.common.world;
+
+import java.util.Random;
+
+import riseautomatons.common.Ids;
+
+import net.minecraft.src.BiomeGenForest;
+import net.minecraft.src.WorldGenerator;
+
+public class BiomeGenTech extends BiomeGenForest {
+
+	WorldGenerator worldGeneratorBigTree = new WorldGenFakeBigTree(false);
+	public BiomeGenTech(int par1) {
+		super(par1);
+		worldGeneratorTrees = new WorldGenFakeTrees(false);
+	}
+
+	@Override
+	public WorldGenerator getRandomWorldGenForTrees(Random par1Random) {
+
+		if (par1Random.nextInt(5) == 0) {
+			return this.worldGeneratorForest;
+		}
+		if (par1Random.nextInt(10) == 0) {
+			return this.worldGeneratorBigTree;
+		}
+
+		return this.worldGeneratorTrees;
+	}
+
+}
