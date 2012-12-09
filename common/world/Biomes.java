43bf3c4ea675008b4554e5a9fef262ba57fe1146
dewfalse <dewfalse@gmail.com>
2012-12-09 20:05:52 +0900

    Techバイオームを登録

--- common/world/Biomes.java
+++ common/world/Biomes.java
@@ -1,9 +1,16 @@
 package riseautomatons.common.world;
 
-import net.minecraft.src.BiomeGenBase;
+import riseautomatons.common.Ids;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.src.BiomeGenBase;
+import net.minecraftforge.common.DimensionManager;
 
 public class Biomes {
+
+	public static final BiomeGenBase giants = (new BiomeGenGiants(42)).setColor(0xff0000).setBiomeName("Giants");
+	//public static final BiomeGenBase autumn = (new BiomeGenAutumn(43)).setColor(0xff0000).setBiomeName("Autumn");
+	//public static final BiomeGenBase windFarm = (new BiomeGenWindFarm(44)).setColor(0xff0000).setBiomeName("Autumn");
+	public static final BiomeGenBase city = (new BiomeGenCity(45)).setColor(0xff0000).setBiomeName("Autumn");
 
 	public static final BiomeGenBase tech = (new BiomeGenTech(46)).setColor(0xff0000).setBiomeName("Tech");
 
@@ -11,6 +18,13 @@ public class Biomes {
 		GameRegistry.addBiome(tech);
 		GameRegistry.registerWorldGenerator(new OreGenerator());
 		GameRegistry.registerWorldGenerator(new TechSurfaceGenerator());
+		GameRegistry.registerWorldGenerator(new TechRuinGenerator());
+		//GameRegistry.addBiome(giants);
+		//GameRegistry.addBiome(autumn);
+		//GameRegistry.addBiome(windFarm);
+		//GameRegistry.addBiome(city);
+		DimensionManager.registerProviderType(Ids.eupraxia, WorldProviderEupraxia.class, false);
+		DimensionManager.registerDimension(Ids.eupraxia, Ids.eupraxia);
 	}
 
 }
