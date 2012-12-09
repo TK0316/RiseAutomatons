96757e764c1cde0f2ec0e2a68e8eff714c689559
dewfalse <dewfalse@gmail.com>
2012-12-09 19:58:26 +0900

    privateな設定値をいじるために継承クラス追加

---
+++ common/world/BiomeDecoratorCustom.java
@@ -0,0 +1,32 @@
+package riseautomatons.common.world;
+
+import net.minecraft.src.BiomeDecorator;
+import net.minecraft.src.BiomeGenBase;
+
+public class BiomeDecoratorCustom extends BiomeDecorator {
+
+	public BiomeDecoratorCustom(BiomeGenBase par1BiomeGenBase) {
+		super(par1BiomeGenBase);
+	}
+
+	public void setTreesPerChunk(int per1) {
+		treesPerChunk = per1;
+	}
+
+	public void setGrassPerChunk(int per1) {
+		grassPerChunk = per1;
+	}
+
+	public void setFlowersPerChunk(int per1) {
+		flowersPerChunk = per1;
+	}
+
+	public void setSandPerChunk(int per1) {
+		sandPerChunk = per1;
+	}
+
+	public void setClayPerChunk(int per1) {
+		clayPerChunk = per1;
+	}
+
+}
