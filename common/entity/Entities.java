00dc3acd4400370b835bde001805c4c37584ea36
dewfalse <dewfalse@gmail.com>
2012-12-09 21:03:54 +0900

    エンティティIDのとりかたが間違ってたので修正

--- common/entity/Entities.java
+++ common/entity/Entities.java
@@ -15,23 +15,23 @@ public class Entities {
 		// Automatons
 		LanguageRegistry.instance().addStringLocalization("entity.Worker.name", "en_US", "Worker");
 		BlockWorker.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityWorker.class, "Worker", BlockWorker.renderId, 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityWorker.class, "Worker", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Sentry.name", "en_US", "Sentry");
 		BlockSentry.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntitySentry.class, "Sentry", BlockSentry.renderId, 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntitySentry.class, "Sentry", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Golem.name", "en_US", "Golem");
 		EntityGolem.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityGolem.class, "Golem", EntityGolem.renderId, 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityGolem.class, "Golem", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.GolemPure.name", "en_US", "GolemPure");
 		EntityGolem.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityGolemPure.class, "GolemPure", EntityGolemPure.renderId, 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityGolemPure.class, "GolemPure", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Watcher.name", "en_US", "Watcher");
 		EntityWatcher.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityWatcher.class, "Watcher", EntityWatcher.renderId, 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityWatcher.class, "Watcher", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
 
 		// Effect
 		EntityFwooshFX.rendererId = ModLoader.getMinecraftInstance().renderEngine
