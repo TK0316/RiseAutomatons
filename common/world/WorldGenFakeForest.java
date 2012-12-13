a33df85b1429a63ea3923ead61ff14615706aca8
dewfalse <dewfalse@gmail.com>
2012-12-13 22:46:00 +0900

    小さな木の生成を追加 #6

--- common/world/WorldGenFakeForest.java
+++ common/world/WorldGenFakeForest.java
@@ -19,6 +19,46 @@ public class WorldGenFakeForest extends WorldGenForest {
 			int par4, int par5) {
 
 		for (int var6 = 0; var6 < 5; ++var6) {
+			int i = par3 + par2Random.nextInt(8) - par2Random.nextInt(8);
+			int j = par4 + par2Random.nextInt(4) - par2Random.nextInt(4);
+			int k = par5 + par2Random.nextInt(8) - par2Random.nextInt(8);
+
+			if (par1World.isAirBlock(i, j, k)) {
+				// height
+				int var10 = 1 + par2Random.nextInt(par2Random.nextInt(10) + 1);
+
+				int length = 0;
+				for (int n = 0; n < var10 && j - n > 0; ++n) {
+					if(par1World.isAirBlock(i, j - n, k)
+							&& par1World.isAirBlock(i + 1, j - n, k)
+							&& par1World.isAirBlock(i - 1, j - n, k)
+							&& par1World.isAirBlock(i, j - n, k + 1)
+							&& par1World.isAirBlock(i, j - n, k - 1)
+							) {
+						length++;
+					}
+					else if(length > 0 && par1World.getBlockId(i, j - n, k) == Block.grass.blockID){
+						//System.out.println("placable");
+						int meta = length < 6 ? length : 5;
+						for (int m = j - n; m < j && m < j - n + meta; ++m) {
+							if(par1World.isAirBlock(i, m, k)) {
+								//par1World.setBlockAndMetadata(var7, var12, var9, Blocks.grower.blockID, meta);
+								par1World.setBlockAndMetadata(i, m, k, Blocks.grower.blockID, meta);
+								//par1World.setBlock(i, m, k, Block.brick.blockID);
+								//System.out.println(String.valueOf(var7) + ", " +  String.valueOf(var8)  + ", " +  String.valueOf(var9));
+							}
+						}
+						if(meta == 5) {
+							par1World.setBlock(i, j - n + meta, k, Blocks.crink.blockID);
+						}
+						break;
+					}
+				}
+
+			}
+		}
+
+		for (int var6 = 0; var6 < 5; ++var6) {
 			int var7 = par3 + par2Random.nextInt(8) - par2Random.nextInt(8);
 			int var8 = par4 + par2Random.nextInt(4) - par2Random.nextInt(4);
 			int var9 = par5 + par2Random.nextInt(8) - par2Random.nextInt(8);
