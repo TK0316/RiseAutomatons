c042c2e5773ec39f407de812276c99b0220cd746
dewfalse <dewfalse@gmail.com>
2012-12-19 21:16:34 +0900

    entityのIDを設定から利用するように修正

--- common/entity/Entities.java
+++ common/entity/Entities.java
@@ -1,8 +1,10 @@
 package riseautomatons.common.entity;
 
-import riseautomatons.common.block.*;
 import net.minecraft.src.ModLoader;
 import net.minecraftforge.client.MinecraftForgeClient;
+import riseautomatons.common.Ids;
+import riseautomatons.common.block.BlockSentry;
+import riseautomatons.common.block.BlockWorker;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Side;
 import cpw.mods.fml.common.asm.SideOnly;
@@ -15,47 +17,47 @@ public class Entities {
 		// Automatons
 		LanguageRegistry.instance().addStringLocalization("entity.Worker.name", "en_US", "Worker");
 		BlockWorker.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityWorker.class, "Worker", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityWorker.class, "Worker", Ids.entityWorkerId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Sentry.name", "en_US", "Sentry");
 		BlockSentry.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntitySentry.class, "Sentry", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntitySentry.class, "Sentry", Ids.entitySentryId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Golem.name", "en_US", "Golem");
 		EntityGolem.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityGolem.class, "Golem", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityGolem.class, "Golem", Ids.entityGolemId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.GolemPure.name", "en_US", "GolemPure");
 		EntityGolem.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityGolemPure.class, "GolemPure", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityGolemPure.class, "GolemPure", Ids.entityGolemPureId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Watcher.name", "en_US", "Watcher");
 		EntityWatcher.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityWatcher.class, "Watcher", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityWatcher.class, "Watcher", Ids.entityWatcherId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Slider.name", "en_US", "Slider");
 		EntitySlider.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntitySlider.class, "Slider", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntitySlider.class, "Slider", Ids.entitySliderId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Bobby.name", "en_US", "Bobby");
 		EntityBobby.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityBobby.class, "Bobby", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityBobby.class, "Bobby", Ids.entityBobbyId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Helios.name", "en_US", "Helios");
 		EntityHelios.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityHelios.class, "Helios", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityHelios.class, "Helios", Ids.entityHeliosId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Factotum.name", "en_US", "Factotum");
 		EntityFactotum.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityFactotum.class, "Factotum", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityFactotum.class, "Factotum", Ids.entityFactotumId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Beacon.name", "en_US", "Beacon");
 		EntityBeacon.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityBeacon.class, "Beacon", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityBeacon.class, "Beacon", Ids.entityBeaconId, 0xCCCCCC, 0xFFFFFF);
 
 		LanguageRegistry.instance().addStringLocalization("entity.Guard.name", "en_US", "Guard");
 		EntityGuard.renderId = RenderingRegistry.getNextAvailableRenderId();
-		EntityRegistry.registerGlobalEntityID(EntityGuard.class, "Guard", EntityRegistry.findGlobalUniqueEntityId(), 0xCCCCCC, 0xFFFFFF);
+		EntityRegistry.registerGlobalEntityID(EntityGuard.class, "Guard", Ids.entityGuardId, 0xCCCCCC, 0xFFFFFF);
 
 		// Effect
 
