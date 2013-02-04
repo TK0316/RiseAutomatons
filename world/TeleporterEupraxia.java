98efabea8a15e38f8e40b637d6764d3afb55d566
dewfalse <dewfalse@gmail.com>
2013-02-04 09:19:45 +0900

    eupraxia移動時に地表に出現するように修正

--- world/TeleporterEupraxia.java
+++ world/TeleporterEupraxia.java
@@ -1,5 +1,6 @@
 package riseautomatons.world;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
@@ -13,6 +14,19 @@ public class TeleporterEupraxia extends Teleporter {
 	@Override
 	public void placeInPortal(Entity par1Entity, double par2, double par4,
 			double par6, float par8) {
+		int x = (int)par1Entity.posX;
+		int y = (int)par1Entity.posY;
+		int z = (int)par1Entity.posZ;
+		for(int j = y; j < 255; ++j) {
+			//Material m = par1Entity.worldObj.getBlockMaterial(x, j - 2, z);
+			//if(m == null) continue;
+			//if(m.isSolid() == false) continue;
+			if(par1Entity.worldObj.getBlockId(x, j - 1, z) != 0) continue;
+			if(par1Entity.worldObj.getBlockId(x, j, z) != 0) continue;
+			if(par1Entity.worldObj.getBlockId(x, j + 1, z) != 0) continue;
+			par1Entity.setLocationAndAngles(x, j + 2, z, 0, 0);
+			break;
+		}
 	}
 
 }
