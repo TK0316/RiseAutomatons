7c15eef4a0e956aac807884a42e6aeae71a44e32
dewfalse <dewfalse@gmail.com>
2013-03-09 02:55:30 +0900

    パルスライフル弾のあたり判定が横にずれるバグを修正

--- item/ItemBlaster.java
+++ item/ItemBlaster.java
@@ -15,12 +15,10 @@ public class ItemBlaster extends Item {
 	}
 
 	@Override
-	public ItemStack onItemRightClick(ItemStack itemstack, World world,
-			EntityPlayer entityplayer) {
-		// if(entityplayer.inventory.consumeInventoryItem(Item.arrow.shiftedIndex))
+	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
+		// if(entityplayer.inventory.consumeInventoryItem(Item.arrow.itemID))
 		// {
-		world.playSoundAtEntity(entityplayer, "automatons.fwoom", 1.0F,
-				1.0F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		world.playSoundAtEntity(entityplayer, "automatons.fwoom", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 0.8F));
 
 		//if (!Universal.improperWorld(world)) {
 			// Entity targetedEntity=entity;
@@ -32,13 +30,13 @@ public class ItemBlaster extends Item {
 			 * / 2.0F)) - (posY + (double)(height )); double d7 =
 			 * targetedEntity.posZ - posZ;
 			 */
-			EntityLaser entityfireball = new EntityLaser(world,
-					entityplayer, vec3d.xCoord * d8, vec3d.yCoord * d8,
-					vec3d.zCoord * d8, 0.2D);
-			entityfireball.posX = entityplayer.posX;// + vec3d.xCoord * d8;
-			entityfireball.posY = entityplayer.posY - 0.75D;
-			entityfireball.posZ = entityplayer.posZ;// + vec3d.zCoord * d8;
-			world.spawnEntityInWorld(entityfireball);
+			EntityLaser entityfireball = new EntityLaser(world,entityplayer, vec3d.xCoord * d8, vec3d.yCoord * d8, vec3d.zCoord * d8, 0.2D);
+			//entityfireball.posX = entityplayer.posX;// + vec3d.xCoord * d8;
+			//entityfireball.posY = entityplayer.posY - 0.75D;
+			//entityfireball.posZ = entityplayer.posZ;// + vec3d.zCoord * d8;
+			if(world.isRemote == false) {
+				world.spawnEntityInWorld(entityfireball);
+			}
 			// world.entityJoinedWorld(new EntityArrow(world, entityplayer));
 		//}
 
