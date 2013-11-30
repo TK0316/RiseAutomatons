f802bc00575f0312f523df105ae05c70582870c6
dewfalse <dewfalse@gmail.com>
2013-11-30 23:16:34 +0900

    不要なコメントの削除

--- entity/EntityTote.java
+++ entity/EntityTote.java
@@ -321,12 +321,10 @@ public class EntityTote extends EntityOwnedBot implements IInventory, IBot {
 	}
 	@Override
 	public boolean isInvNameLocalized() {
-		// TODO 自動生成されたメソッド・スタブ
 		return false;
 	}
 	@Override
 	public boolean isStackValidForSlot(int i, ItemStack itemstack) {
-		// TODO 自動生成されたメソッド・スタブ
 		return false;
 	}
 	@Override
